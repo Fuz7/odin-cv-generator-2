b2cc9fb87ce307ed4f8e3dada202abca8cbe3f08
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2024-08-16 19:18:38 +0800

    Add addbarlistbutton setState logic

--- src/components/BarList.jsx
+++ src/components/BarList.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
-function BarList({ active,buttonName,barName,data,setData }) {
-  const workData = data.find((bar)=> bar.barName === barName)
+import { v4 as uuidv4 } from 'uuid';
+
+function BarList({ active, buttonName, barName, data, setData }) {
+  const workData = data.find((bar) => bar.barName === barName);
   return (
     <>
       <div
@@ -17,30 +19,75 @@ function BarList({ active,buttonName,barName,data,setData }) {
               : 'barListContainer__separator'
           }
         ></div>
-        {data.map((bar) =>{
-          if(bar.barName === barName && bar.list.length > 0){
-            return bar.list.map((item) =>{
-              return (<div className="barList" key={barName + 'barList'+item[0].id}>
-                <p className="barList__title" key={barName +'barList__title' +item[0].id}>{item[0].data}</p>
-                <button onClick={stopEventPropagation} className="barList__button "key={barName+"barList__button"+item[0].id}></button>
-              </div>)
-            })
+        {data.map((bar) => {
+          if (bar.barName === barName && bar.list.length > 0) {
+            return bar.list.map((item) => {
+              return (
+                <div className="barList" key={barName + 'barList' + item[0].id}>
+                  <p
+                    className="barList__title"
+                    key={barName + 'barList__title' + item[0].id}
+                  >
+                    {item[0].data}
+                  </p>
+                  <button
+                    onClick={stopEventPropagation}
+                    className="barList__button "
+                    key={barName + 'barList__button' + item[0].id}
+                  ></button>
+                </div>
+              );
+            });
           }
         })}
         {data.map((bar) => {
-          if(bar.barName === barName && bar.list.length > 0){
-            return <div key={bar.barName + "separator"} className="barListContainer__separator--active"></div>
+          if (bar.barName === barName && bar.list.length > 0) {
+            return (
+              <div
+                key={bar.barName + 'separator'}
+                className="barListContainer__separator--active"
+              ></div>
+            );
           }
         })}
-        
-       <button onClick={stopEventPropagation} className="barListContainer__addButton">{buttonName}</button> 
+
+        <button
+          onClick={(event) => addBarList(event, barName, data, setData)}
+          className="barListContainer__addButton"
+        >
+          {buttonName}
+        </button>
       </div>
     </>
   );
 }
 
-function stopEventPropagation(e){  
-  e.stopPropagation()
+function addBarList(e, barName, data, setData) {
+  stopEventPropagation(e);
+
+  setData((data) => {
+    return data.map((bar) => {
+      if (bar.barName === barName) {
+        bar.list = [
+          ...bar.list,
+          [
+            {
+              id: uuidv4(),
+              type: 'Company Name',
+              title: '',
+              data: 'Mapua Malayan',
+            },
+          ],
+        ];
+
+      } 
+      return bar
+    });
+  });
+}
+
+function stopEventPropagation(e) {
+  e.stopPropagation();
 }
 
 export default BarList;
